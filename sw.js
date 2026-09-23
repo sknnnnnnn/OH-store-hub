ff5a3eea4db8940620017096ea39e86c664ca2c9
sknnnnnnn <164526710+sknnnnnnn@users.noreply.github.com>
2026-09-23 13:39:13 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "oh-store-hub-v14";
+const CACHE_NAME = "oh-store-hub-v15";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", event => {
